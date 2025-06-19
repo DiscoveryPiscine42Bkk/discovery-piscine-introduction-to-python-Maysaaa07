7ea8578448cb90d8df105b4a2844981a417a983a
Maysaaa07 <68030285@kmitl.ac.th>
2025-06-19 13:03:37 +0000

    play_with_arrays

--- cell03/ex03/advanced_mult.py
+++ cell03/ex03/advanced_mult.py
@@ -2,7 +2,7 @@ def main() :
     i = 0
     while i <= 10:
         j = 0
-        output = f="Table de {i}:"
+        output = f="Table de :"
         while j <= 10:
             output += f" {i * j}"
             j += 1
